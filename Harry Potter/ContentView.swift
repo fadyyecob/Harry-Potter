8641346754f230d03ec5180bb266e6d3d66d2956
fadyyecob <fransfadi@hotmail.com>
2021-11-13 22:43:58 +0100

    Initial Commit

---
+++ Harry Potter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Harry Potter
+//
+//  Created by Fady Yecob on 13/11/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Harry PotterTests/Harry_PotterTests.swift
@@ -0,0 +1,33 @@
+//
+//  Harry_PotterTests.swift
+//  Harry PotterTests
+//
+//  Created by Fady Yecob on 13/11/2021.
+//
+
+import XCTest
+@testable import Harry_Potter
+
+class Harry_PotterTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
